bd1b5f39c5fcca537a73746f3294dbd2b01f9ca4
fachrul pbm <fachrul.pbm@gmail.com>
2015-10-10 19:19:20 +0700

    Tugas dikirim ke pak hari, ok syiipp

--- Source code/MazeRat.cpp
+++ Source code/MazeRat.cpp
@@ -1,114 +1,127 @@
-#include<stdio.h>
- 
-// Maze size
-#define N 4 
- 
-bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N]);
- 
-/* A utility function to print solution matrix sol[N][N] */
-void printSolution(int sol[N][N])
-{
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            printf(" %d ", sol[i][j]);
-        printf("\n");
-    }
-}
- 
-/* A utility function to check if x,y is valid index for N*N maze */
-bool isSafe(int maze[N][N], int x, int y)
-{
-    // if (x,y outside maze) return false
-    if(x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 1)
-        return true;
- 
-    return false;
-}
- 
-/* This function solves the Maze problem using Backtracking.  It mainly uses
-solveMazeUtil() to solve the problem. It returns false if no path is possible,
-otherwise return true and prints the path in the form of 1s. Please note that
-there may be more than one solutions, this function prints one of the feasible
-solutions.*/
-bool solveMaze(int maze[N][N])
-{
-    int sol[N][N] = { {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}
-    };
- 
-    if(solveMazeUtil(maze, 0, 0, sol) == false)
-    {
-        printf("Solusi Gak Ada");
-        return false;
-    }
- 
-    printSolution(sol);
-    return true;
-}
- 
-/* A recursive utility function to solve Maze problem */
-bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N])
-{
-    // if (x,y is goal) return true
-    if(x == N-1 && y == N-1)
-    {
-        sol[x][y] = 1;
-        return true;
-    }
- 
-    // Check if maze[x][y] is valid
-    if(isSafe(maze, x, y) == true)
-    {
-        // mark x,y as part of solution path
-        sol[x][y] = 1;
- 
-        /* Move forward in x direction */
-        if (solveMazeUtil(maze, x+1, y, sol) == true)
-            return true;
- 
-        /* If moving in x direction doesn't give solution then
-           Move down in y direction  */
-        if (solveMazeUtil(maze, x, y+1, sol) == true)
-            return true;
- 
-        /* If none of the above movements work then BACKTRACK: 
-            unmark x,y as part of solution path */
-        sol[x][y] = 0;
-        return false;
-    }   
- 
-    return false;
-}
- 
-// driver program to test above function
+#include <iostream>
+#define ROW 7
+#define COL 7
+using namespace std;
+
+// Fachrul Pralienka Bani Muhamad (5115201044)
+
+void mazeSekarang ( char maze[ROW][COL] , int a, int b,int x,int y ,int i,int j ); 
+void cetakMaze ( const char[ROW][COL] );
+bool validMove ( const char [ROW][COL], int, int ); 
+bool notStuck ( char maze[ROW][COL],int x,int y, int i,int j );
+
 int main()
 {
-    int maze[N][N]  =  { 
-	    {1, 1, 0, 0},
-        {1, 1, 1, 1},
-        {1, 1, 1, 1},
-        {1, 1, 0, 0}
-    };
-    
-    printf("Labirin Yang Ada \r\n");
-    for (int i = 0;i<4; i++ ){
-    	for (int j = 0;j<4; j++ ){
-    		if (maze[i][j]==1){
-    			printf(" _ ");
-			}else {
-				printf(" X ");
-			}
-    		//printf("%2i ",maze[i][j]);	
-    	}
-    	printf("\r\n");
-	}
- 	printf("\r\n");
- 	printf("Penyelesaian \r\n");
-    solveMaze(maze);
-    getchar();
-    return 0;
+	char maze[ ROW ][ COL ]={		
+		{'.','O','.','.','.','.','.'},
+		{'.','.','.','O','.','O','O'},
+		{'.','O','.','O','.','.','.'},
+		{'.','O','.','O','.','O','O'},
+		{'.','O','.','O','.','O','.'},
+		{'.','O','.','O','.','.','.'},
+		{'.','O','.','O','O','.','.'}
+	}; 
+	int xStart, yStart, x, y;
+	xStart=0;
+	yStart=0; 
+	x = xStart;  // starting row
+	y = yStart;  // starting col
+   
+   mazeSekarang(maze,xStart,yStart,x,y,-1,-1  );
+   return 0;
 }
 
+void mazeSekarang(char maze[ROW][COL], int a, int b,int x,int y,int i,int j ){	
+	bool c = false; 
+	maze[ x ][ y ] = 'x';  // insert X at current location
+	cout<<x;
+	cout<<y;	
+	cout<<(" \n" );
+	cetakMaze( maze );
+	cout <<endl<<"Tekan enter untuk melihat langkah tikus selanjutnya!";
+    cin.get();
+	
+    if (x== ROW-1 && y== COL-1){   //goal
+    		cetakMaze(maze);
+			cout <<endl<<"Alhamdulillah.. Tikus berhasil sampai tujuan :D\n\n"; 
+			return ;   // maze is complete
+	}else {  
+			c = true;
+			// move right
+			if ( validMove( maze, x , y + 1  ) ) { 
+				if ( x != i || y + 1 != j ) {
+					mazeSekarang( maze, a, b, x , y + 1, x, y );
+   					return; 
+				}else { 
+					if ( notStuck (maze,x,y,i,j)) {
+					}else{
+						maze[ x ][ y ] = 'F';
+						mazeSekarang( maze, a, b, x , y + 1, x, y );
+   						return; 
+					}
+				}									
+            }  
+			// move down
+	 		if ( validMove( maze, x + 1 , y  ) ) { 
+	 		 	if ( x + 1 != i || y != j ) {
+	 		 		mazeSekarang( maze,a,b, x + 1, y, x , y );
+            		return;
+				}else { 
+					if ( notStuck (maze,x,y,i,j)) {
+					}else{
+						maze[ x ][ y ] = 'F';
+						mazeSekarang( maze , a , b , x + 1, y, x , y );
+            			return;
+					}
+				}				
+			}             
+			// move up
+			if ( validMove( maze, x - 1 , y   )  ) {
+     			if ( x-1 != i || y != j ) {
+     				mazeSekarang( maze,a,b, x - 1, y, x,y );
+            		return;
+				}else { 
+					if ( notStuck (maze,x,y,i,j)) {
+					}else{
+						maze[ x ][ y ] = 'F';
+						mazeSekarang( maze , a , b , x - 1, y, x,y );
+            			return;
+					}
+				} 
+            } 
+			// move left
+			if ( validMove( maze, x , y-1 ) ) {
+            	if ( x != i || y-1 != j ) {
+            			mazeSekarang( maze, a , b , x, y - 1,x,y );
+						return ;
+					}else { 
+					if ( notStuck (maze,x,y,i,j)) {
+					}else{
+						maze[ x ][ y ] = 'F';
+						mazeSekarang( maze, a , b , x, y - 1,x,y );
+						return ;
+					}
+				}						
+			}										 
+    }
+}
+
+bool validMove( const char maze[ROW][ COL ], int r, int c ){	
+   	return ( r >= 0 && r <= ROW-1 && c >= 0 && c <= COL-1 && maze[ r ][ c ] != 'O' && maze[ r ][ c ] != 'F' );
+}
+
+void cetakMaze( const char maze[ROW][ COL ] ) {	
+ 	system("cls");
+   	for ( int x = 0; x < ROW; ++x ) {
+      for ( int y = 0; y < ROW; ++y )
+         cout << maze[ x ][ y ] << ' ';
+      cout << '\n';
+   	}   	    
+}
+
+bool notStuck(char maze[ROW][COL],int x,int y, int i,int j ){	
+	return(	(validMove( maze, x , y + 1  )&&( x != i || y + 1 != j ))  ||
+	 (validMove( maze, x + 1 , y )&&( x + 1 != i || y != j ) )|| 
+	 (validMove( maze, x - 1 , y )&& ( x-1 != i || y != j ))||
+	 (validMove( maze, x , y-1 )&&( x != i || y-1 != j ))) ;
+}
